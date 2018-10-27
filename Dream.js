272871de2dc94fd4775d67447514da597f0f30ad
spaminggbots <44527121+spaminggbots@users.noreply.github.com>
2018-10-27 17:20:19 +0300

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,30 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Script");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("498233231922036756")
+setInterval(function() {
+channel.send(`youtube`);
+}, 25)
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);
